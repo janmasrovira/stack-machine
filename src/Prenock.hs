1c29a89b441909adc711831cabf8fe78d703a596
Paul Cadman <git@paulcadman.dev>
2023-12-21 11:11:48 +0000

    wip this is working

--- src/Prenock.hs
+++ src/Prenock.hs
@@ -4,9 +4,6 @@ import Base
 import Juvix.Compiler.Nockma.Evaluator
 import Juvix.Compiler.Nockma.Language
 import Juvix.Compiler.Nockma.Pretty
-import Stacks (Stacks)
-import Stacks qualified
-import Value
 
 type Prenock = [Instruction]
 
@@ -28,7 +25,7 @@ numStacks :: (Integral a) => a
 numStacks = fromIntegral (length (allElements @StackName))
 
 data StackOp
-  = StackPush Natural
+  = StackPush (Term Natural)
   | StackPop
   deriving stock (Show)
 
@@ -55,19 +52,61 @@ data Instruction
   deriving stock (Show)
 
 data Compiler m a where
-  Push :: StackName -> Natural -> Compiler m ()
+  Push :: StackName -> Term Natural -> Compiler m ()
   Pop :: StackName -> Compiler m ()
+  Call :: Text -> Natural -> Compiler m ()
   Increment :: StackName -> Compiler m ()
   Branch :: m () -> m () -> Compiler m ()
 
 -- | The path to the head of a named stack
 stackPath :: StackName -> Path
-stackPath s = replicate (fromIntegral (fromEnum s)) R ++ finalPath
-  where
-    finalPath :: Path
-    finalPath
-      | fromEnum s == numStacks - 1 = []
-      | otherwise = [L]
+stackPath s = indexStack (fromIntegral (fromEnum s))
+
+indexStack :: Natural -> Path
+indexStack idx = replicate idx R ++ [L]
+
+indexInPath :: Path -> Natural -> Path
+indexInPath p idx = p ++ indexStack idx
+
+indexInStack :: StackName -> Natural -> Path
+indexInStack s idx = stackPath s ++ indexStack idx
+
+--- [ code [0 0] ]
+--- [8 [9 ADDR STDLIB_ADDR] [9 2 [10 [6 ARGS] [0 2]]]]
+
+-- [push
+--   [quote [[suc [@ RL]] 123 0]]
+--   [call L @ L]
+-- ]
+
+-- [[[suc [@ RL]] 123 0] [value-stack call-stack....]  ]
+
+-- [call L [quote [[suc [@ RL]] 123 0]]]
+
+-- [call L [@ (stackPath FunctionLibrary PATH_TO_FUNCTION)]]
+
+-- [value-stack call-stack]
+
+-- [push
+--   [quote [[@ RL] 123 0]]
+--   [@ S]
+-- ]
+
+-- Proposal:
+
+--   [stuff, arg1, ...., argn, f] => [stuff, arg1, ... argn, return] => [stuff, arg1]
+
+--   1. Copy the value stack to the head of the call stack
+--   2. Pop the arguments from the stack in 1.
+--   2. Push the function to value stack
+--   3. Pop/Write the arguments into the function
+--   4. Evaluate the function
+--   5. Push the head of the value stack to the stack in 1.
+--   6. Replace the value stack with the stack in 1
+--   7. Pop the call stack
+
+funIncrement :: Term Natural
+funIncrement = (OpInc # (OpAddress # [R, L])) # ((0 :: Natural) # (0 :: Natural))
 
 -- | Construct a path rooted at he head of a named stack
 pathInStack :: StackName -> Path -> Path
@@ -94,10 +133,46 @@ seqTerms = foldl' step (OpAddress # emptyPath) . reverse
     step :: Term Natural -> Term Natural -> Term Natural
     step acc t = OpSequence # t # acc
 
-runCompiledNock :: (MonadIO m) => Sem '[Compiler] () -> m ()
+-- initStacks :: [(Text, Term Natural)] -> HashMap Text Path
+
+makeList :: [Term Natural] -> Term Natural
+makeList ts = foldr1 (#) (ts ++ [TermAtom nockNil])
+
+remakeList :: [Term Natural] -> Term Natural
+remakeList ts = foldr1 (#) (ts ++ [OpQuote # nockNil'])
+
+nockNil' :: Term Natural
+nockNil' = TermAtom nockNil
+
+initStack :: [Term Natural] -> Term Natural
+initStack defs = makeList (initSubStack <$> allElements)
+  where
+    initSubStack :: StackName -> Term Natural
+    initSubStack = \case
+      ValueStack -> nockNil'
+      CallStack -> nockNil'
+      TempStack -> nockNil'
+      StandardLibrary -> nockNil'
+      FunctionsLibrary -> makeList defs
+
+functions :: [(Text, Term Natural)]
+functions = [("increment", funIncrement), ("lala", toNock (1 :: Natural))]
+
+functionLocations :: HashMap Text Path
+functionLocations =
+  hashMap
+    [ (n, pathInStack FunctionsLibrary (replicate i R))
+      | (i, (n, _)) <- zip [0 ..] functions
+    ]
+
+runCompiledNock :: (MonadIO m) => Sem '[Compiler, Reader (HashMap Text Path)] () -> m ()
 runCompiledNock s = do
-  let t = run (execCompiler s)
-      stack = foldr1 (#) (reverse (replicate numStacks (toNock (0 :: Natural))))
+  let t =
+        run
+          . runReader functionLocations
+          . execCompiler
+          $ s
+      stack = initStack (snd <$> functions)
       evalT =
         run
           . runError @(ErrNockNatural Natural)
@@ -109,10 +184,10 @@ runCompiledNock s = do
       Left e -> error (show e)
       Right res -> putStrLn (ppTrace res)
 
-execCompiler :: Sem (Compiler ': r) a -> Sem r (Term Natural)
+execCompiler :: (Member (Reader (HashMap Text Path)) r) => Sem (Compiler ': r) a -> Sem r (Term Natural)
 execCompiler = fmap fst . runCompiler
 
-runCompiler :: Sem (Compiler ': r) a -> Sem r (Term Natural, a)
+runCompiler :: (Member (Reader (HashMap Text Path)) r) => Sem (Compiler ': r) a -> Sem r (Term Natural, a)
 runCompiler sem = do
   (ts, a) <- runOutputList (re sem)
   return (seqTerms ts, a)
@@ -126,35 +201,58 @@ reInstruction = reinterpretH $ \case
   Pop _ -> output (InstructionStack StackPop) >>= pureT
   Increment _ -> output (InstructionOp Incr) >>= pureT
 
-re :: Sem (Compiler ': r) a -> Sem (Output (Term Natural) ': r) a
+re :: (Member (Reader (HashMap Text Path)) r) => Sem (Compiler ': r) a -> Sem (Output (Term Natural) ': r) a
 re = reinterpretH $ \case
-  Push s n -> output (pushOnStack s (OpQuote # n)) >>= pureT
-  Pop s -> output (popStack s) >>= pureT
+  Push s n -> outputT (pushOnStack s (OpQuote # n))
+  Pop s -> outputT (popStack 1 s)
+  Call funName funArgsNum -> do
+    funPath <- fromJust <$> asks @(HashMap Text Path) (^. at funName)
+    -- outputT (pushOnStack CallStack (OpAddress # pathInStack ValueStack (replicate funArgsNum R)))
+    outputT (pushOnStack ValueStack
+              (OpAddress # funPath)
+             --(OpQuote #
+
+              -- (OpReplace # (([R, L] # (OpAddress # pathInStack ValueStack [L])) # (OpAddress # funPath)))
+
+             --)
+
+            )
+    -- outputT (pushOnStack ValueStack (OpReplace # (([R, L] # (OpAddress # pathInStack ValueStack [L])) # OpAddress # funPath)))
+
+
+    ----- stack: [[h stuff] .... [.... [code args]] ...]
+    ----
+
+
+    --- [code [h args]]
+    -- outputT (pushOnStack ValueStack (OpAddress # funPath))
+
   Increment s ->
-    output
+    outputT
       ( replaceOnStack
           s
           (OpInc # (OpAddress # pathInStack s [L]))
       )
-      >>= pureT
   Branch t f -> do
     termT <- runT t >>= raise . execCompiler . (pop ValueStack >>)
     termF <- runT f >>= raise . execCompiler . (pop ValueStack >>)
-    output (OpIf # (OpAddress # pathInStack ValueStack [L]) # termT # termF) >>= pureT
+    outputT (OpIf # (OpAddress # pathInStack ValueStack [L]) # termT # termF)
   where
+    outputT :: (Functor f, Member (Output (Term Natural)) r) => Term Natural -> Sem (WithTactics e f m r) (f ())
+    outputT = pureT <=< output
+
     pushOnStack :: StackName -> Term Natural -> Term Natural
     pushOnStack s t = OpPush # t # topStack s
 
     replaceOnStack :: StackName -> Term Natural -> Term Natural
     replaceOnStack s t = OpPush # t # replaceTopStack s
 
-    popStack :: StackName -> Term Natural
-    popStack sn =
-      foldr1
-        (#)
+    popStack :: Natural -> StackName -> Term Natural
+    popStack n sn =
+      remakeList
         [ let p = stackPath s
               a
-                | sn == s = p ++ [R]
+                | sn == s = p ++ replicate n R
                 | otherwise = p
            in OpAddress # a
           | s <- allElements
@@ -162,12 +260,13 @@ re = reinterpretH $ \case
 
     -- Reconstruct the value-stack / call-stack cell by moving the global head to the
     -- respective stack head.
+    --- [h [s1 s1 s3 nil]]
+    --- [ s1 .. [h si] ... sn nil]
     topStack :: StackName -> Term Natural
     topStack sn =
-      foldr1
-        (#)
+      remakeList
         [ let p = OpAddress # (R : stackPath s)
-            in if
+           in if
                 | sn == s -> (OpAddress # [L]) # p
                 | otherwise -> p
           | s <- allElements
@@ -175,8 +274,7 @@ re = reinterpretH $ \case
 
     replaceTopStack :: StackName -> Term Natural
     replaceTopStack sn =
-      foldr1
-        (#)
+      remakeList
         [ let p = R : stackPath s
            in if
                 | sn == s -> (OpAddress # [L]) # (OpAddress # (p ++ [R]))
@@ -184,64 +282,73 @@ re = reinterpretH $ \case
           | s <- allElements
         ]
 
-runNock :: [Instruction] -> IO (Either Text Natural)
-runNock = fmap (fmap (^. valueNat)) . runM . runError . Stacks.runStacks . runProgram
-  where
-    runProgram :: forall r'. (Members '[Stacks] r') => [Instruction] -> Sem r' Value
-    runProgram instructions = do
-      valueStackId <- Stacks.newStack
-      instructionsStackId <- Stacks.newStack
-      pcStackId <- Stacks.newStack
-      firstInstructionPath <- Stacks.stackBottomPath instructionsStackId
-      Stacks.pushStack pcStackId (ValueNat (encodePath firstInstructionPath ^. encodedPath))
-      pcPath <- Stacks.stackBottomPath pcStackId
-      forM_ instructions $ \i -> Stacks.pushStack instructionsStackId (encodeInstruction i)
-      let incrementPc :: Sem r' ()
-          incrementPc = do
-            ValueNat pc :: Value <- Stacks.readValue pcPath
-            pc' :: Path <- Stacks.nextPath (decodePath' (EncodedPath pc))
-            Stacks.popStack pcStackId
-            void (Stacks.pushStack pcStackId (ValueNat (encodePath pc' ^. encodedPath)))
+-- runNock :: [Instruction] -> IO (Either Text Natural)
+-- runNock = fmap (fmap (^. valueNat)) . runM . runError . Stacks.runStacks . runProgram
+--   where
+--     runProgram :: forall r'. (Members '[Stacks] r') => [Instruction] -> Sem r' Value
+--     runProgram instructions = do
+--       valueStackId <- Stacks.newStack
+--       instructionsStackId <- Stacks.newStack
+--       pcStackId <- Stacks.newStack
+--       firstInstructionPath <- Stacks.stackBottomPath instructionsStackId
+--       Stacks.pushStack pcStackId (ValueNat (encodePath firstInstructionPath ^. encodedPath))
+--       pcPath <- Stacks.stackBottomPath pcStackId
+--       forM_ instructions $ \i -> Stacks.pushStack instructionsStackId (encodeInstruction i)
+--       let incrementPc :: Sem r' ()
+--           incrementPc = do
+--             ValueNat pc :: Value <- Stacks.readValue pcPath
+--             pc' :: Path <- Stacks.nextPath (decodePath' (EncodedPath pc))
+--             Stacks.popStack pcStackId
+--             void (Stacks.pushStack pcStackId (ValueNat (encodePath pc' ^. encodedPath)))
 
-          interpretLoop :: Sem r' ()
-          interpretLoop = do
-            ValueNat pc :: Value <- Stacks.readValue pcPath
-            let curPc = decodePath' (EncodedPath pc)
-            whenM (decodeBool <$> Stacks.pathExists curPc) $ do
-              i <- decodeInstruction <$> Stacks.readValue curPc
-              case i of
-                InstructionOp Incr -> do
-                  v <- Stacks.popStack valueStackId
-                  void (Stacks.pushStack valueStackId (incr v))
-                InstructionStack sop -> case sop of
-                  StackPop -> void (Stacks.popStack valueStackId)
-                  StackPush n -> void (Stacks.pushStack valueStackId (ValueNat n))
-                InstructionBranch b -> undefined
-              incrementPc
-              interpretLoop
-      interpretLoop
-      Stacks.popStack valueStackId
+--           interpretLoop :: Sem r' ()
+--           interpretLoop = do
+--             ValueNat pc :: Value <- Stacks.readValue pcPath
+--             let curPc = decodePath' (EncodedPath pc)
+--             whenM (decodeBool <$> Stacks.pathExists curPc) $ do
+--               i <- decodeInstruction <$> Stacks.readValue curPc
+--               case i of
+--                 InstructionOp Incr -> do
+--                   v <- Stacks.popStack valueStackId
+--                   void (Stacks.pushStack valueStackId (incr v))
+--                 InstructionStack sop -> case sop of
+--                   StackPop -> void (Stacks.popStack valueStackId)
+--                   StackPush n -> void (Stacks.pushStack valueStackId (ValueNat n))
+--                 InstructionBranch b -> undefined
+--               incrementPc
+--               interpretLoop
+--       interpretLoop
+--       Stacks.popStack valueStackId
 
-prog1 :: Sem '[Compiler] ()
+pushNat :: (Member Compiler r) => StackName -> Natural -> Sem r ()
+pushNat s n = push s (toNock n)
+
+prog1 :: Sem '[Compiler, Reader (HashMap Text Path)] ()
 prog1 = do
-  push ValueStack 13
-  push TempStack 20
-  push CallStack 666
-  increment CallStack
-  push ValueStack 13
-  increment CallStack
-  pop ValueStack
-  push ValueStack 50
-  push CallStack 33
-  push CallStack 44
-  pop CallStack
-  pop ValueStack
-  increment CallStack
-  push ValueStack 0
-  branch (push ValueStack 99) (push ValueStack 77)
+  pushNat ValueStack 111
+  pushNat ValueStack 222
+  pushNat ValueStack 333
+  --call "increment" 1
+  -- push FunctionsLibrary funIncrement
+  -- pushNat ValueStack 13
+  -- pushNat TempStack 20
+  -- pushNat CallStack 666
+  -- increment CallStack
+  -- pushNat ValueStack 13
+  -- increment CallStack
+  -- pop ValueStack
+  -- pushNat ValueStack 50
+  -- pushNat CallStack 33
+  -- pushNat CallStack 44
+  -- pop CallStack
+  -- pop ValueStack
+  -- increment CallStack
+  pushNat ValueStack 0
+  branch (pushNat ValueStack 99) (pushNat ValueStack 77)
 
-prog :: [Instruction]
-prog = serializeCompiler prog1
+initProg :: Sem '[Compiler] ()
+initProg = do
+  push FunctionsLibrary funIncrement
 
 instance Semigroup EncodedPath where
   a <> b = encodePath (decodePath' a <> decodePath' b)
@@ -261,20 +368,20 @@ incrOpCode = magicBase + 1
 pushOpCode :: Natural
 pushOpCode = magicBase + 2
 
-encodeInstruction :: Instruction -> Value
-encodeInstruction =
-  ValueNat . \case
-    InstructionOp Incr -> incrOpCode
-    InstructionStack s -> case s of
-      StackPop -> popOpCode
-      StackPush n -> pushOpCode + n
+-- encodeInstruction :: Instruction -> Value
+-- encodeInstruction =
+--   ValueNat . \case
+--     InstructionOp Incr -> incrOpCode
+--     InstructionStack s -> case s of
+--       StackPop -> popOpCode
+--       StackPush n -> pushOpCode + n
 
-decodeInstruction :: Value -> Instruction
-decodeInstruction (ValueNat n)
-  | n == popOpCode = InstructionStack StackPop
-  | n == incrOpCode = InstructionOp Incr
-  | n >= pushOpCode = InstructionStack (StackPush (n - pushOpCode))
-  | otherwise = error "bad encoding of instruction"
+-- decodeInstruction :: Value -> Instruction
+-- decodeInstruction (ValueNat n)
+--   | n == popOpCode = InstructionStack StackPop
+--   | n == incrOpCode = InstructionOp Incr
+--   | n >= pushOpCode = InstructionStack (StackPush (n - pushOpCode))
+--   | otherwise = error "bad encoding of instruction"
 
 readPath :: (Members '[Compiler] r) => Path -> Sem r ()
 readPath p = do
